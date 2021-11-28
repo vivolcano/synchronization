946d21a90385663f21392ec12faee31e09cfc8e7
Viktor Loskutov <vikandloskutov@gmail.com>
2021-11-28 15:18:39 +0300

    add task_2 with Lock

--- src/main/java/Seller.java
+++ src/main/java/Seller.java
@@ -1,44 +1,58 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class Seller {
 
     private final Store store;
+    private static final Lock LOCK = new ReentrantLock(true);
+    private static final Condition CONDITION = LOCK.newCondition();
 
     public Seller(Store shop) {
         this.store = shop;
     }
 
-    public synchronized void sellCar() {
+    public void sellCar() {
         try {
+            LOCK.lock();
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон.");
             while (store.getAuto().isEmpty()) {
                 System.out.println(Thread.currentThread().getName() + ", извините, машин пока нет, ждем поступление.");
-                wait();
+                CONDITION.await();
             }
             int waitingTime = 3500;
             Thread.sleep(waitingTime);
-            System.out.println(Thread.currentThread().getName() + " уехал на новенькой машине, удачи!"
-                    + store.getAuto().get(0));
+            System.out.println(Thread.currentThread().getName() + " уехал на новенькой "
+                    + store.getAuto().get(0) + "! Удачи ему!");
+
+            store.getAuto().remove(0);
+            CONDITION.signalAll();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
 
-        store.getAuto().remove(0);
-        notifyAll();
+        } finally {
+            LOCK.unlock();
+        }
+        Thread.currentThread().interrupt();
     }
 
-    public synchronized void makeCar() {
+    public void makeCar() {
         try {
+            LOCK.lock();
             while (store.getAuto().isEmpty()) {
                 System.out.println(Thread.currentThread().getName() + " выпустил 1 авто!");
                 store.getAuto().add(new Car());
-                notify();
-                wait();
+                CONDITION.signal();
+                CONDITION.await();
             }
             int timeToMakeNewCar = 2500;
             Thread.sleep(timeToMakeNewCar);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            LOCK.unlock();
         }
     }
 }
