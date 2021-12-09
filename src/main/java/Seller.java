0a7db981fac4eff9e08ea484568188c147980f56
Viktor Loskutov <vikandloskutov@gmail.com>
2021-12-09 20:26:18 +0300

    корректировка согласно замечания преподователя

--- src/main/java/Seller.java
+++ src/main/java/Seller.java
@@ -6,14 +6,15 @@ class Seller {
         this.store = shop;
     }
 
-    public synchronized void sellCar() {
+    public synchronized Car sellCar() {
         try {
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон.");
-            while (store.getAuto().isEmpty()) {
-                System.out.println(Thread.currentThread().getName() + ", извините, машин пока нет, ждем поступление.");
+            while (store.getAuto().size() == 0) {
+                System.out.println(Thread.currentThread().getName()
+                        + ", извините, машин пока нет, ждем поступление.");
                 wait();
             }
-            int waitingTime = 3500;
+            int waitingTime = 1500;
             Thread.sleep(waitingTime);
             System.out.println(Thread.currentThread().getName() + " уехал на новенькой машине "
                     + store.getAuto().get(0) + "! Удачи ему!");
@@ -21,22 +22,21 @@ class Seller {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        store.getAuto().remove(0);
-        notifyAll();
+        return store.getAuto().remove(0);
     }
 
     public synchronized void makeCar() {
         try {
-            while (store.getAuto().isEmpty()) {
-                System.out.println(Thread.currentThread().getName() + " выпустил 1 авто!");
+            while (store.getAuto().size() == 0) {
+                System.out.println("[ПРОЗВОДСТВО]");
+                int timeToMakeNewCar = 2000;
+                Thread.sleep(timeToMakeNewCar);
                 store.getAuto().add(new Car());
+                System.out.println(Thread.currentThread().getName() + " выпустил 1 авто!");
                 notify();
-                wait();
+                int productionBreak = 1500;
+                wait(productionBreak);
             }
-            int timeToMakeNewCar = 2500;
-            Thread.sleep(timeToMakeNewCar);
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
